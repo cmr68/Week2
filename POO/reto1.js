78efdbdeb665c8443d7d1502705e3d6dbdf02be0
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-07-19 13:04:03 +0200

    class Person

--- POO/reto1.js
+++ POO/reto1.js
@@ -1,1 +1,9 @@
-console.log("hola");
+// Crea la clase Person de la manera más completa que se te ocurra. Su constructor debe
+// tener como mínimo 2 parámetros de entrada.
+class Person{
+    constructor(name, surname, brith){
+        this.nombre = name;
+        this.apellido = surname;
+        this.anyoNacimiento = brith;
+    }
+}
