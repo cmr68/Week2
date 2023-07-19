f676602609b919934fda0615c6c4ac33c0b9027b
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-07-19 17:05:59 +0200

    con Contact

--- POO/person.js
+++ POO/person.js
@@ -20,19 +20,19 @@ class Person{
     }
     //imprime todos los atributos
     printAll(){
-        console.log(`- ${this.name} - ${this.surname} - ${this.yearOfBirth} - ${this.age} -`);
+        console.log(`- ${this.name} - ${this.surname} - ${this.yearOfBirth} - ${this.age} - ${this.hobbies}`);
     }
     printhobbies(){
         console.log(`${this.hobbies}`);
     }
 }
 module.exports = {Person};
-let hobbiesP = ["Nadar","Bucear","Pescar"];
-let person1 = new Person("Patricia","Alvarez",1994,hobbiesP);
-console.log(person1.name,"tiene un IMC de:",person1.imc(1.78,69));
-console.log(person1.name,person1.surname, "tiene", person1.age,"años");
-person1.printAll();
-person1.printhobbies();
+// let hobbiesP = ["Nadar","Bucear","Pescar"];
+// let person1 = new Person("Patricia","Alvarez",1994,hobbiesP);
+// console.log(person1.name,"tiene un IMC de:",person1.imc(1.78,69));
+// console.log(person1.name,person1.surname, "tiene", person1.age,"años");
+// person1.printAll();
+// person1.printhobbies();
 
 // Reto 7
 // • Crear una clase que se llame Contacts que tenga un atributo que sea un array de objetos de la clase 
@@ -40,5 +40,14 @@ person1.printhobbies();
 // • El constructor de la clase Contacts no debe tener parámetros de entrada pero debe inicializar el array.
 // • Crear un método llamado printPersons que imprima cada uno de los atributos de cada objeto Person.
 class Contacts{
-
+    constructor(){
+        this.persona = [new Person("Mari","Martinez",1956,["andar","pintar","teatro"]),
+                       new Person("Noel","Giner",2005,["leer","jugar","fiesta"])];
+    }
+    //Métodos
+    printPersons(){
+        console.log(this.persona);
+    }
 }
+let c = new Contacts();
+c.printPersons();
