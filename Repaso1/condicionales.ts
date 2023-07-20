ba8ff13d6782efd304dab69ec0e2cd8d99c54349
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-07-20 15:49:34 +0200

    nose

---
+++ Repaso1/condicionales.ts
@@ -0,0 +1,11 @@
+// let signoZodiac = (dia: number, mes: string)=>{
+//     if(dia < 1 && 
+//         (dia > 31 && 
+//             (mes == "Abril" ||
+//             mes == "Junio" ||
+//             mes == "Septiembre") ||
+//             mes == "Noviembre"){
+//         if((dia >= 21 && mes == "enero") )
+
+//     }
+// } 
